77b8d4628b82f3a1c90a50645fee87a34a1c192f
WebUser <WebUser@webuser.local>
2017-05-18 10:17:10 +0800

    first commit

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by WebUser on 17/4/11.
+//  Copyright © 2017年 tigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
